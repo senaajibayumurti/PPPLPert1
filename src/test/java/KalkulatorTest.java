69fa53ddf6419f9435a5877a6888efa40b5b2224
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-02-29 13:38:49 +0700

    added initial var

--- src/test/java/KalkulatorTest.java
+++ src/test/java/KalkulatorTest.java
@@ -1,17 +1,21 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KalkulatorTest {
+    int nilai01=2, nilai02=3;
 
     @Test
     void penjumlahanNilaiPositif() {
-        Kalkulator kalkulator = new Kalkulator(2,3);
-        int expectedHasil = kalkulator.penjumlahan();
-        assertEquals(5,expectedHasil);
+        Kalkulator kalkulator = new Kalkulator(nilai01,nilai02);
+        int hasil = kalkulator.penjumlahan();
+        assertEquals(5,hasil);
     }
     @Test
     void penjumlahanNilaiNegatif() {
-        System.out.println("sukses niggatif");
+        Kalkulator kalkulator = new Kalkulator(nilai01,nilai02);
+        int hasil = kalkulator.penjumlahan();
+        assertEquals(5,hasil);
     }
 }
